67699174670fb50a2326249ab33347379ca1e961
Jake Zaia <jlz2369@g.rit.edu>
2020-02-15 14:53:57 -0500

    Initial commit

---
+++ antiviralvoyager.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import sys
+sys.path.insert(0,"/var/www/antiviralvoyager/")
+from theinternetofthings import app as application
---
+++ antiviralvoyager/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
+import os, json
+
+app = Flask(__name__)
+DIR = os.path.dirname(__file__) or '.'
+app.secret_key = os.urandom()
+
+@app.route("/")
+def root():
+    return render_template("base.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
